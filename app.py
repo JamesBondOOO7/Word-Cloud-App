c993f50f6ebcc3baf538d035d113def7642da34b
Manan Arora <54953734+JamesBondOOO7@users.noreply.github.com>
2020-07-16 14:36:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("WORD CLOUD")
+
+def draw_wordcloud(background_color, max_words, img, width, height, text):
+    wc = WordCloud(
+    background_color=background_color,
+    max_words=max_words,
+    stopwords = set(STOPWORDS),
+    mask = np.array(img)
+    )
+
+    fig = plt.figure()
+    wc.generate(text)
+    fig.set_figwidth(width) # set width
+    fig.set_figheight(height) # set height
+    plt.imshow(wc, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot()
+
+
+def main():
+    st.sidebar.title("🌟 Select your favourite Image 🌟")
+
+    img = st.sidebar.selectbox("Mask Images for Word Cloud",("CLOUD","INDIA","SPY","STAR","UPVOTE","LEAF","BIRD"))
+
+    st.sidebar.header("Preview")
+    img1 = Image.open("cloud.png")
+    image = img1
+    st.sidebar.image(img1, width=250, caption="CLOUD")
+
+    img2 = Image.open("india.png")
+    st.sidebar.image(img2, width=250, caption="INDIA")
+
+    img3 = Image.open("spy.png")
+    st.sidebar.image(img3, width=250, caption="SPY")
+
+    img4 = Image.open("star.png")
+    st.sidebar.image(img4, width=250, caption="STAR")
+
+    img5 = Image.open("upvote.png")
+    st.sidebar.image(img5, width=250, caption="UPVOTE")
+
+    img6 = Image.open("leaf.png")
+    st.sidebar.image(img6, width=250, caption="LEAF")
+
+    img7 = Image.open("bird.png")
+    st.sidebar.image(img7, width=250, caption="BIRD")
+
+    if img == "INDIA":
+        image = img2
+
+    elif img == "SPY":
+        image = img3
+
+    elif img == "STAR":
+        image = img4
+
+    elif img == "UPVOTE":
+        image = img5
+
+    elif img == "LEAF":
+        image = img6
+
+    elif img == "BIRD":
+        image = img7
+
+    else:
+        image = img1
+
+    st.header("Select Background Color")
+    bgc = st.radio("Background Color", ("White","Black","Yellow","Red","Blue","Green","Orange","Violet"), key="bgc")
+
+    st.header("Select Maximim Number of Words")
+    max_words = st.slider("Max Words" , 1000, 3000 , key="max_words")
+
+    st.header("Set Width and Height")
+    width = st.slider("Width" , 10, 50 , key="width")
+    height = st.slider("Height" , 10, 60 , key="height")
+
+    msg = st.text_area("Enter the Text","Type Here...")
+
+    if st.button("Submit",key="b1"):
+        draw_wordcloud(bgc, max_words, image, width, height, msg)
+
+
+
+if __name__ == '__main__':
+    main()
